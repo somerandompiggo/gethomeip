eb52e6867877e73540043ae5992ff4b437586ffd
somerandompiggo <somerandompiggo@gmail.com>
2022-07-08 08:23:56 +0200

    from server LMAO

--- client/client.py
+++ client/client.py
@@ -17,17 +17,25 @@ def startupchk():
         print("Done. Running client.")
 
 def loadconf():
-    f = open("conf.json", "r")
-    return(json.loads(f.read()))
+    f = open("conf.json", "r") 
+    j = json.loads(f.read())
+    if not "/set" in j["uri"]:
+        if not j["uri"][-1] == "/":
+            j["uri"] = j["uri"] + "/set"
+        else:
+            j["uri"] = j["uri"] + "set"
+    return j
 
 def mainloop():
     config = loadconf()
     headers = {'User-Agent': 'Mozilla/5.0'}
     payload = {'password': config["password"]}
-    while 0 < 1:
-        session = requests.Session()
-        session.post(config["uri"], headers=headers,data=payload)
-        time.sleep(15)
+    while True:
+        try:
+            session = requests.Session()
+            session.post(config["uri"], headers=headers,data=payload)
+            time.sleep(15)
+        except: pass
 
 startupchk()
 mainloop()
--- server/server.py
+++ server/server.py
@@ -9,20 +9,22 @@ def startupchk():
         open("password").read()
     except:
         print("Password file not found. Please enter a password.")
-        f = open("password", "w")
+        f = open("./password", "w")
         f.write(hashlib.sha256(getpass.getpass("Password: ").encode("UTF-8")).hexdigest())
         f.close()
         print("Done. Running Flask server")
 
 def getpwhash():
-    f = open("password", "r")
-    return(f.read())
+    f = open("./password", "r")
+    return f.read().strip()
 
 def chkpass(password):
+    print(hashlib.sha256(password.encode("UTF-8")).hexdigest())
+    print(getpwhash())
     if (hashlib.sha256(password.encode("UTF-8")).hexdigest() == getpwhash()):
-        return(True)
+        return True
     else:
-        return(False)
+        return False
 
 startupchk()
 
@@ -30,30 +32,30 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return(send_file("index.html"))
+    return send_file("./index.html")
 
 @app.route("/style")
 def style():
-    return(send_file("css/style.css"))
+    return send_file("./css/style.css")
 
 @app.route("/access", methods=["POST"])
 def access():
     global homeip
     if (chkpass(request.form.get("password"))):
         if (homeip == ""):
-            return(render_template("access.html", ip=("The client has not sent an IP yet!")))
+            return render_template("./access.html", ip=("The client has not sent an IP yet!"))
         else:
-            return(render_template("access.html", ip=("Your home IP is: " + homeip)))
+            return render_template("./access.html", ip=("Your home IP is: " + homeip))
     else:
-        return(render_template("access.html", ip="That password is incorrect, please try again."))
+        return render_template("./access.html", ip="That password is incorrect, please try again.")
 
 @app.route("/set", methods=["POST"])
 def set():
     global homeip
     if (chkpass(request.form.get("password"))):
         homeip = request.remote_addr
-        return()
+        return
     else:
-        return()
+        return
 
-app.run()
+app.run(host='0.0.0.0')
